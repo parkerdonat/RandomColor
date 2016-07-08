ccb652ef4bc82494b9b6226655592d5707b6c6f4
Parker Donat <parkerdonat@gmail.com>
2016-07-08 17:05:16 -0600

    Initial Commit

---
+++ RandomColor/RandomColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by Parker Donat on 7/8/16.
+//  Copyright © 2016 Parker Donat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
